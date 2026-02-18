edc9985abd4489a1c05ad9ceb94cada52bb8f443
Vértessy Márton Dávid <vertessy.marton@gmail.com>
2026-02-18 14:57:09 +0100

    feat(auth): add login endpoint with bycript compare and jwt token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -32,19 +32,47 @@ router.post("/register", (req, res) => {
         const result = insertUser.run(username, hashedPassword)
         res.sendStatus(201)
     }catch(err){
-
         if(err.message.includes("UNIQUE")) {
             return res.sendStatus(409)
         }
        
         console.log(err.message)
-        res.sendStatus(503)
+        res.sendStatus(5001)
        
     }
 })
 
 router.post("/login", (req, res) => {
+    const {username, password} = req.body
 
+    if (!username || !password) {
+        return res.status(400).json({ error: "username and password are required" })
+    }
+    if (typeof username !== "string" || typeof password !== "string") {
+        return res.status(400).json({ error: "username and password must be strings" })
+    }
+
+    try{
+        const getUser= db.prepare(`SELECT * FROM users WHERE username = ?`)
+        const user = getUser.get(username)
+
+        if (!user) { return res.status(404).json({message: "Invalid credentials"}) }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+
+        if (!passwordIsValid) { return res.status(401).json({message: "Invalid credentials"}) }
+
+        if (passwordIsValid) { 
+            const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: "1h"})
+            return res.json({ token })
+        }
+
+    }catch(err){
+        console.log(err.message)
+        res.sendStatus(500)
+    }
+    
+    
 })
 
 
