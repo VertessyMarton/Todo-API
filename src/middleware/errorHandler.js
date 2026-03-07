06ac2675a819c554a0a9c27da4779e4215f7b088
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-03-07 01:32:15 +0100

    feat: add global error handler

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,45 @@
+import AppError from "../utils/AppError.js"
+import { mapPrismaError } from "../utils/prismaErrorMap.js";
+
+export default function errorHandler(err, req, res, next) {
+    if(res.headersSent) {
+        return next(err)
+    }
+
+    if (err?.type === "entity.parse.failed") {
+    err = AppError.badRequest("Malformed JSON body");
+    }
+
+    const isProd = process.env.NODE_ENV === "production";
+
+    err = mapPrismaError(err);
+
+    let statusCode = 500;
+    let message = "Internal Server Error";
+    let code = "INTERNAL_ERROR";
+    let details;
+
+    if(err instanceof AppError) {
+        statusCode = err.statusCode
+        message = err.message
+        code = err.code || code
+        details = err.details
+    }
+
+    console.error({
+        name: err.name,
+        message: err.message,
+        stack: err.stack,
+        method: req.method,
+        path: req.originalUrl
+    })
+
+    res.status(statusCode).json({
+    error: {
+      message,
+      code,
+      ...(details && { details }),
+      ...(isProd ? {} : { stack: err.stack }),
+    },
+  });
+}
