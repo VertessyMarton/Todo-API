a50bbe7fecc73975cba18357fda6f3ba6645adc2
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-05-01 00:03:41 +0200

    feat: set up register page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full',
+  },
+];
--- frontend/src/app/features/auth/pages/register/register.component.ts
+++ frontend/src/app/features/auth/pages/register/register.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../core/services/auth.service';
+import { email } from '@angular/forms/signals';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  constructor(private auth: AuthService) {}
+  email = '';
+  password = '';
+  confirmPassword = '';
+
+  onSubmit() {
+    this.auth
+      .register({
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+      })
+      .subscribe({
+        next: (res) => console.log(res),
+        error: (err) => console.error(err),
+      });
+  }
+}
