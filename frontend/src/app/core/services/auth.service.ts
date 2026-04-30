d1c62601800cd3f68f22aaca687b3267aadc2bf8
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-05-01 00:01:54 +0200

    feat: add register to auth.service

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,6 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+type RegisterPaylaod = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {}
+export class AuthService {
+  private readonly apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  register(payload: RegisterPaylaod) {
+    return this.http.post(`${this.apiUrl}/auth/register`, payload);
+  }
+}
