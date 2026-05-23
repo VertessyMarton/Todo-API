3e242e0a4fa4d64316d19f2845fc9ae0d0b63abb
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-05-23 10:37:29 +0200

    feat: add todo methods

--- frontend/src/app/core/services/todo.service.ts
+++ frontend/src/app/core/services/todo.service.ts
@@ -1,6 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+export type Todo = {
+  id: number;
+  task: string;
+  completed: boolean;
+  userId: number;
+};
+
+type TodosResponse = {
+  todos: Todo[];
+};
 
 @Injectable({
   providedIn: 'root',
 })
-export class TodoService {}
+export class TodoService {
+  private readonly apiUrl = 'http://localhost:3000/todos';
+
+  constructor(private http: HttpClient) {}
+
+  getTodos() {
+    return this.http.get<TodosResponse>(this.apiUrl);
+  }
+
+  createTodo(task: string) {
+    return this.http.post<Todo>(this.apiUrl, { task });
+  }
+
+  updateTodo(id: number, completed: boolean) {
+    return this.http.put<Todo>(`${this.apiUrl}/${id}`, { completed });
+  }
+
+  deleteTodo(id: number) {
+    return this.http.delete<{ message: string }>(`${this.apiUrl}/${id}`);
+  }
+}
