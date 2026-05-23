992bc462850406f0bf7f137ecd7dcb03964a72e4
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-05-23 08:46:19 +0200

    feat: add todos route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/pages/register/register.component';
 import { LoginComponent } from './features/auth/pages/login/login.component';
+import { TodosComponent } from './features/todos/pages/todos/todos.component';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   {
@@ -10,6 +12,11 @@ export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'todos',
+    component: TodosComponent,
+    canActivate: [authGuard],
   },
   {
     path: '',
